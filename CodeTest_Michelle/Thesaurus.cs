ab32a37a869c4ae3a9261923747cbb17f069dd22
michelle opey <mipanowicz@gmail.com>
2021-06-16 14:23:25 +0200

    add synonyms

--- CodeTest_Michelle/Thesaurus.cs
+++ CodeTest_Michelle/Thesaurus.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace CodeTest_Michelle
 {
 
     class Thesaurus : IThesaurus
     {
-        List<string> dictionary = new();
+        public Dictionary<string, List<string>> dictionary = new();
         
         public void AddSynonyms(IEnumerable<string> synonyms)
         {
-            throw new System.NotImplementedException();
+            List<string> synonymsList = synonyms.ToList();
+
+            string key = synonymsList[0];
+            synonymsList.Remove(synonymsList[0]);
+
+            dictionary.Add(key, synonymsList);
+           
         }
 
         public IEnumerable<string> GetSynonyms(string word)
@@ -20,6 +28,7 @@ namespace CodeTest_Michelle
         public IEnumerable<string> GetWords()
         {
             throw new System.NotImplementedException();
+
         }
     }
 }
