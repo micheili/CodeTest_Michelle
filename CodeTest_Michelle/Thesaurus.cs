9511a8c33b993ff7e456449b7fec2169f0483dd2
michelle opey <mipanowicz@gmail.com>
2021-06-16 16:43:18 +0200

    input validation passing

--- CodeTest_Michelle/Thesaurus.cs
+++ CodeTest_Michelle/Thesaurus.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System;
+using System.Text.RegularExpressions;
+
 
 namespace CodeTest_Michelle
 {
@@ -12,12 +14,39 @@ namespace CodeTest_Michelle
         public void AddSynonyms(IEnumerable<string> synonyms)
         {
             List<string> synonymsList = synonyms.ToList();
+            string key;
 
-            string key = synonymsList[0];
-            synonymsList.Remove(synonymsList[0]);
+            if (CheckIfWordIsAccepted(synonymsList[0]))
+            {
+                key = synonymsList[0];
+                synonymsList.Remove(synonymsList[0]);
 
-            dictionary.Add(key, synonymsList);
-           
+                if (dictionary.ContainsKey(key))
+                {
+                    //if it does, check if the other synonyms exists as values
+                    //add other synonyms if they don't
+
+                    foreach (var synonym in synonymsList)
+                    {
+                        if (!dictionary[key].Contains(synonym))
+                            dictionary[key].Add(synonym);
+                    }
+                }
+                else
+                {
+                    dictionary.Add(key, synonymsList);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input.");
+            }  
+        }
+
+        public bool CheckIfWordIsAccepted(string word)
+        {
+            string wordRegex = @"^[a-zA-Z]+$";
+            return Regex.IsMatch(word, wordRegex);
         }
 
         public IEnumerable<string> GetSynonyms(string word)
@@ -27,8 +56,15 @@ namespace CodeTest_Michelle
 
         public IEnumerable<string> GetWords()
         {
-            throw new System.NotImplementedException();
+            List<string> wordsOfDictionary = new();
 
+            foreach (var word in dictionary)
+            {
+                string key = word.Key;
+                List<string> values = word.Value;
+                //wordsOfDictionary.Add(key + (value => values );
+            }
+            return wordsOfDictionary;
         }
     }
 }
